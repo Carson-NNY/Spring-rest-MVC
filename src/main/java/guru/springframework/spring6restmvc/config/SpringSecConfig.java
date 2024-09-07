97eee77ddc8fd0403eb6f0884dddff4e03a6cdf5
Guanhong Liu <guanhongliu45@gmail.com>
2024-09-07 10:20:59 -0400

    finished integrating openAPI management

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import javax.swing.*;
 
 // setup as the oauth2 resource server
+@Profile("!test") // tell Spring Boot to ignore this config when testing
 @Configuration
 public class SpringSecConfig {
 
---
+++ src/test/java/guru/springframework/spring6restmvc/contoller/BeerControllerRestAssuredTest.java
@@ -0,0 +1,69 @@
+package guru.springframework.spring6restmvc.contoller;
+
+import com.atlassian.oai.validator.OpenApiInteractionValidator;
+import com.atlassian.oai.validator.restassured.OpenApiValidationFilter;
+import com.atlassian.oai.validator.whitelist.ValidationErrorsWhitelist;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
+
+import static com.atlassian.oai.validator.whitelist.rule.WhitelistRules.messageHasKey;
+import static io.restassured.RestAssured.given;
+
+
+
+@ActiveProfiles("test") // set the profile to test, and disable the spring security configuration for test, otherwise there are 2 beans
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(BeerControllerRestAssuredTest.TestConfig.class)
+@ComponentScan(basePackages = "guru.springframework.spring6restmvc")
+public class BeerControllerRestAssuredTest {
+
+    // use Swagger validation filter,  to check if the incoming requests match the oa3.yml (OpenAPI  specification)
+    OpenApiValidationFilter  filter = new OpenApiValidationFilter(OpenApiInteractionValidator
+            .createForSpecificationUrl("oa3.yml")
+            .withWhitelist(ValidationErrorsWhitelist.create()
+                    .withRule("Ignored date format", // 因为date format会始终不match, ignore this error
+                    messageHasKey("validation.response.body.schema.format.date-time")))
+            .build());
+
+    @Configuration
+    public static class TestConfig {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+            http.authorizeHttpRequests(authorize -> {
+                authorize.anyRequest().permitAll();
+            });
+
+            return http.build();
+        }
+    }
+
+    @LocalServerPort
+    Integer localPort;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = localPort;
+    }
+
+    @Test
+    void testListBeers() {
+        given().contentType(ContentType.JSON)
+                .when()
+                .filter(filter)
+                .get("/api/v1/beer")
+                .then()
+                .assertThat().statusCode(200);
+    }
+}
